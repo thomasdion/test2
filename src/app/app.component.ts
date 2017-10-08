e5e588f798fb25f6da866c67d017e314b8f6e8b0
Dionisis Thomas <thomas.dionisis81@gmail.com>
2017-10-09 00:08:13 +0300

    Heroes Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['app/app.component.css'],
+  providers:[ HeroService]
 })
 export class AppComponent implements OnInit {
 
   heroes: Hero[];
   sHero: Hero;
-  constructor() {
-  }
+  constructor(private heroService: HeroService) { }
+
   ngOnInit() {
     // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     // Add 'implements OnInit' to the class.
-    let Heroes: Hero[] = [];
-    let test2 = new Hero(11, 'Mr. Nice');
-    Heroes.push(test2);
-    test2 = new Hero(12, 'Narco');
-    Heroes.push(test2);
-    test2 = new Hero(13, 'Bombaco');
-    Heroes.push(test2);
-    test2 = new Hero(14, 'Celeritas');
-    Heroes.push(test2);
-    test2 = new Hero(15, 'Magneta');
-    Heroes.push(test2);
-    test2 = new Hero(16, 'RubberMan');
-    Heroes.push(test2);
-    test2 = new Hero(17, 'Dynama');
-    Heroes.push(test2);
-    test2 = new Hero(18, 'Dr IQ');
-    Heroes.push(test2);
-    test2 = new Hero(19, 'Magma');
-    Heroes.push(test2);
-    test2 = new Hero(20, 'Tornado');
-    Heroes.push(test2);
-    this.heroes = Heroes;
+     this.getHeroes();
     // console.log('SDFSDFSDFSDF' + this.heroes);
 
   }
@@ -44,7 +26,9 @@ export class AppComponent implements OnInit {
     this.sHero = hero;
     // console.log('SDFSDFSDFSDF' + this.sHero);
   }
-
+    public getHeroes(): void {
+      this.heroService.getHeroesSlowley().then(heroes => this.heroes = HEROES);
+  }
  }
 
 
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+@Injectable()
 export class HeroService {
 
-
-
-
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  };
+  getHeroesSlowley(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(this.getHeroes()), 2000);
+    });  }
 }
